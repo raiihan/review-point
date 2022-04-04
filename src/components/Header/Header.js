01c9cda4c467f03d2ae23737c10dbe006600de4a
silvaan <s.siilvan@yahoo.com>
2022-04-04 09:16:30 +0600

    Home Product section added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,17 +4,17 @@ import CustomLink from '../CustomLink/CustomLink';
 
 const Header = () => {
     return (
-        <nav className='bg-white shadow-xl text-blue-500 sm:flex h-20 justify-between items-center px-28 '>
+        <nav className='bg-white shadow-xl text-blue-500 text-center w-full overflow-hidden sm:flex h-40 sm:h-20 justify-between items-center px-28 '>
             <div>
-                <Link to={'/'}><p className='font-mono text-2xl font-semibold italic hover:text-blue-700'>Reviews-Point</p></Link>
+                <Link to={'/'}><p className='font-mono w-full text-xl sm:text-2xl font-semibold italic hover:text-blue-700'>Reviews-Point</p></Link>
             </div>
-            <div className='uppercase'>
+            <div className='uppercase text-center  sm:flex justify-end'>
 
-                <CustomLink className='ml-3 font-medium hover:font-semibold hover:text-blue-700' to="/">Home</CustomLink>
-                <CustomLink className='ml-3 font-medium hover:font-semibold hover:text-blue-700' to="/reviews">Reviews</CustomLink>
-                <CustomLink className='ml-3 font-medium hover:font-semibold hover:text-blue-700' to={'/dashboard'}>Dashboard</CustomLink>
-                <CustomLink className='ml-3 font-medium hover:font-semibold hover:text-blue-700' to={'/blogs'}>Blogs</CustomLink>
-                <CustomLink className='ml-3 font-medium hover:font-semibold hover:text-blue-700' to={'/about'}>About</CustomLink>
+                <CustomLink className='ml-3 block font-medium hover:font-semibold hover:text-blue-700' to="/">Home</CustomLink>
+                <CustomLink className='ml-3 block font-medium hover:font-semibold hover:text-blue-700' to="/reviews">Reviews</CustomLink>
+                <CustomLink className='ml-3 block font-medium hover:font-semibold hover:text-blue-700' to={'/dashboard'}>Dashboard</CustomLink>
+                <CustomLink className='ml-3 block font-medium hover:font-semibold hover:text-blue-700' to={'/blogs'}>Blogs</CustomLink>
+                <CustomLink className='ml-3 block font-medium hover:font-semibold hover:text-blue-700' to={'/about'}>About</CustomLink>
             </div>
         </nav>
     );
